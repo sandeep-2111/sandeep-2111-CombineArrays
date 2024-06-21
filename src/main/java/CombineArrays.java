baf06ec94da42bbae2a01a711f6e0f9f599197d9
Vengal Reddy Sandeep <vengalreddysandeep@gmail.com>
2024-06-21 21:28:26 +0530

    Done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,17 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int m=arr1.length+arr2.length;
+        int[] com=new int[m];
+        for(int i=0;i<m;i++){
+            if(i<arr1.length){
+                com[i]=arr1[i];
+            }
+            else{
+                com[i]=arr2[i-arr1.length];
+            }
+        }
 
-        return null;
+        return com;
     }
 }
